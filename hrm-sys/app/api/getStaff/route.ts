093aaf004ed2e82dc7bcfdb8629a3eb4c0f802e9
eaglementality <jesseowusuyeboah99@gmail.com>
2024-09-24 17:23:14 +0000

    changes

--- hrm-sys/app/api/getStaff/route.ts
+++ hrm-sys/app/api/getStaff/route.ts
@@ -3,13 +3,13 @@ import prisma from "../../../prisma/client";
 
 
 export async function GET(
-  req: NextApiRequest,
-  res: NextApiResponse
+  // req: NextApiRequest,
+  // res: NextApiResponse
 ) {
   try {
     const data = await prisma.post.findMany();
     return Response.json(data)
   } catch (err) {
-    return res.status(500).json(err);
+    return Response.json(err);
   }
 }
